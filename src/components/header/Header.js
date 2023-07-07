a5d4d824e2f167db4bba799e532b6bcf572b6e92
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-07-07 01:26:03 -0300

    logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
 //import "./Header.scss";
-import "./Header.css"
+import "./Header.css";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="header-container">
         <div className="header-navbar">
           <div className="top-bar">
-            <img className="img-header-logo" src={logo} alt=""/>
+            <img className="img-header-logo" src={logo} alt="" />
             <a className="title" href="!#">
               Estudio Juridico Integral
             </a>
@@ -24,17 +24,32 @@ const Header = () => {
               <a href="#abogados">Abogados</a>
             </div>
           </div>
-
         </div>{" "}
         {/*  Navbar */}
         <div className="message">
           <h1>Dr Jorge Cancio & Asociados</h1>
           <p>
-            En nuestro estudio jurídico especializado en derecho integral, nos enorgullece ofrecer un enfoque completo y personalizado para satisfacer las necesidades legales de nuestros clientes. Con años de experiencia en diversas áreas del derecho, nuestro equipo de abogados altamente capacitados se compromete a brindar asesoramiento legal sólido y representación de calidad en cada caso que atendemos. Nos dedicamos a comprender a fondo las circunstancias únicas de nuestros clientes y a diseñar estrategias jurídicas efectivas que busquen proteger sus derechos e intereses. Nuestro objetivo es proporcionar un servicio legal excepcional, basado en la ética, la excelencia profesional y la confianza mutua con nuestros clientes. Estamos aquí para apoyarte en cada paso del proceso legal, brindándote orientación experta y representación confiable en todos los aspectos de tu caso.
+            En nuestro estudio jurídico, ofrecemos soluciones legales integrales
+            y expertas para tus necesidades legales. Nuestro equipo de abogados
+            altamente calificados y experimentados está comprometido en
+            brindarte asesoramiento legal personalizado y efectivo en una amplia
+            gama de áreas legales. Desde derecho civil y penal hasta derecho
+            laboral y de familia, contamos con la experiencia necesaria para
+            representarte y proteger tus intereses. Nuestra pasión por la
+            justicia y la excelencia en el servicio al cliente nos distingue.
+            Confía en nosotros para obtener resultados favorables. ¡Contáctanos
+            hoy mismo para una consulta!
           </p>
-          <Button onClick={
-            () => window.open("https://api.whatsapp.com/send?phone=+5491157541353&text=Hola!%20Quiero%20agendar%20una%20cita%20con%20el%20Dr.%20Cancio%20&%20Asociados", "_blank")
-          } className="button" color="green">
+          <Button
+            onClick={() =>
+              window.open(
+                "https://api.whatsapp.com/send?phone=+5491157541353&text=Hola!%20Quiero%20agendar%20una%20cita%20con%20el%20Dr.%20Cancio%20&%20Asociados",
+                "_blank"
+              )
+            }
+            className="button"
+            color="green"
+          >
             <Icon size="big" name="whatsapp" /> Contactanos
           </Button>
         </div>{" "}
