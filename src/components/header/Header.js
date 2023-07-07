bcbdb0b0562bc4e479ab3809d2664a15cfb71a37
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-07-07 01:06:54 -0300

    logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
         </div>{" "}
         {/*  Navbar */}
         <div className="message">
-          <h1>Dr Cancio & Asociados</h1>
+          <h1>Dr Jorge Cancio & Asociados</h1>
           <p>
             En nuestro estudio jurídico especializado en derecho integral, nos enorgullece ofrecer un enfoque completo y personalizado para satisfacer las necesidades legales de nuestros clientes. Con años de experiencia en diversas áreas del derecho, nuestro equipo de abogados altamente capacitados se compromete a brindar asesoramiento legal sólido y representación de calidad en cada caso que atendemos. Nos dedicamos a comprender a fondo las circunstancias únicas de nuestros clientes y a diseñar estrategias jurídicas efectivas que busquen proteger sus derechos e intereses. Nuestro objetivo es proporcionar un servicio legal excepcional, basado en la ética, la excelencia profesional y la confianza mutua con nuestros clientes. Estamos aquí para apoyarte en cada paso del proceso legal, brindándote orientación experta y representación confiable en todos los aspectos de tu caso.
           </p>
