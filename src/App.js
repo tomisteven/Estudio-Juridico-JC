1ad9b230b16530c5587d1e7813654726dfe832f6
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-07-16 19:29:25 -0300

    robots

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/Header'
 import Properties from './components/properties/Properties';
 import HowItWork from './components/how-it-work/How-it-work';
 import Testmonials from './components/testimonials/testimonials';
-/* import Articles from './components/articles/Articles.js'; */
+/*  import Articles from './components/articles/Articles.js'; */
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Header/>
         <AboutUs/>
         <Properties/>
-        {/* <Articles/> */}
+{/*        <Articles/> */}
         <HowItWork/>
         <Testmonials/>
         <Footer/>
--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -5,11 +5,10 @@ import article2 from "../../assets/article2.jpg";
 import article3 from "../../assets/article3.jpg";
 import { Button, Icon, Rating } from "semantic-ui-react";
 
-
 export default function Articles() {
   const articles = [
     {
-      id: 1,
+      id: 0,
       title: "¿Para que sirve una declaratoria de 1 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -21,7 +20,7 @@ export default function Articles() {
       rating: 4,
     },
     {
-      id: 2,
+      id: 1,
       title: "¿Para que sirve una declaratoria de 2 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -33,7 +32,7 @@ export default function Articles() {
       rating: 4,
     },
     {
-      id: 3,
+      id: 2,
       title: "¿Para que sirve una declaratoria de 3 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -45,7 +44,7 @@ export default function Articles() {
       rating: 4,
     },
     {
-      id: 4,
+      id: 3,
       title: "¿Para que sirve una declaratoria de 4 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -57,7 +56,7 @@ export default function Articles() {
       rating: 4,
     },
     {
-      id: 5,
+      id: 4,
       title: "¿Para que sirve una declaratoria de 5 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -69,7 +68,7 @@ export default function Articles() {
       rating: 4,
     },
     {
-      id: 6,
+      id: 5,
       title: "¿Para que sirve una declaratoria de 6 ?",
       description:
         "Cuando una persona fallece, se inicia la sucesion a fin de determinar quienes seran los herederos de esa persona o a fin de evaluar la validez del testamento.",
@@ -82,28 +81,17 @@ export default function Articles() {
     },
   ];
   const [articleActive, setArticleActive] = useState(articles[0]);
- // const [classActive, setClassActive] = useState("article-active");
+  // const [classActive, setClassActive] = useState("article-active");
 
   const handleArticle = (dir) => {
-    if(dir){
-      console.log("Izquierda");
-      if (articleActive.id - 1 > 1) {
-        console.log(articles[articleActive].id);
-        setArticleActive(articles[articleActive.id - 1]);
-      } else {
-        console.log(articleActive.id);
-        setArticleActive(articles[articles]);
-      }
-    }
-    else{
-      console.log("Derecha");
-      if (articleActive.id + 1 > articles.length) {
-        console.log(articles[0].id);
-        setArticleActive(articles[0]);
-      } else {
-        console.log(articleActive.id);
-        setArticleActive(articles[articleActive.id]);
-      }
+    if (dir) {
+      articleActive.id === 0
+        ? setArticleActive(articles[articles.length - 1])
+        : setArticleActive(articles[articleActive.id - 1]);
+    } else {
+      articleActive.id === articles.length - 1
+        ? setArticleActive(articles[0])
+        : setArticleActive(articles[articleActive.id + 1]);
     }
   };
 
@@ -111,10 +99,7 @@ export default function Articles() {
     <section>
       <h2 className="title-articles">Articulos Utiles</h2>
       <div class="cont-articles">
-        <Button
-          className="btn-articles"
-          onClick={() => handleArticle(true)}
-        >
+        <Button className="btn-articles" onClick={() => handleArticle(true)}>
           <Icon name="arrow left" size="small" />
         </Button>
         <div class="card-articles">
@@ -180,10 +165,7 @@ export default function Articles() {
             </div>
           </div>
         </div>
-        <Button
-          className="btn-articles"
-          onClick={() => handleArticle(false)}
-        >
+        <Button className="btn-articles" onClick={() => handleArticle(false)}>
           <Icon name="arrow right" size="small" />
         </Button>
       </div>
