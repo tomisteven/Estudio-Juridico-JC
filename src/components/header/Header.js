6b413c456d18b77679e598632c072ced5cccb963
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-07-17 00:41:55 -0300

    robots

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -84,7 +84,7 @@ const Header = () => {
               icon="whatsapp"
               color="green"
               onClick={() =>
-                window.open("https://goo.gl/maps/r383EhBJgmErtzyi6", "_blank")
+                window.open("https://wa.me/1157541353?text=¡Hola!%20Queria%20agendar%20una%20cita%20con%20el%20Dr%20Cancio", "_blank")
               }
             />
             <Button
